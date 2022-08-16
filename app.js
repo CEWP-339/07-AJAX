8d73e5ed411eada5c8cce72af97c294c2b8ca151
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-08-15 21:24:12 -0400

    promise methods

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
-const getPosition = opts => {
+const getPosition = (opts) => {
   const promise = new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
-      success => {
+      (success) => {
         resolve(success);
       },
-      error => {
+      (error) => {
         reject(error);
       },
       opts
@@ -16,10 +16,10 @@ const getPosition = opts => {
   return promise;
 };
 
-const setTimer = duration => {
+const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('Done!');
+      resolve("Done!");
     }, duration);
   });
   return promise;
@@ -35,29 +35,34 @@ async function trackUserHandler() {
   }
   console.log(timerData, positionData);
 
-    // .then(posData => {
-    //   positionData = posData;
-    //   return setTimer(2000);
-    // })
-    // .then(data => {
-    //   console.log(data, positionData);
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // });
+  // .then(posData => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // })
+  // .then(data => {
+  //   console.log(data, positionData);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
 
   // setTimer(1000).then(() => {
   //   console.log("Timer Done!");
   // });
 
   console.log("Getting Position Data....");
-
 }
 
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
 
-// let result = 0;
-// for (let i = 0; i < 1000000000; i++) {
-//   result += i;
-// }
-// console.log(result);
+// Promise.race([getPosition(), setTimer(5000)]).then((data) => {
+//   console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then((data) => {
+//   console.log(data);
+// });
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then((data) => {
+  console.log(data);
+});
